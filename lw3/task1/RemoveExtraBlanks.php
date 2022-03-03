9de5acb3b4962773512009dbbc458068438e0675
Vlad <osokin-vlad.osokin@yandex.ru>
2022-03-03 04:16:38 +0300

    Lw 3

---
+++ lw3/task1/RemoveExtraBlanks.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/plain');
+function RemoveExtraBlanks(string $text): ?string
+{
+    $text = trim($text, ' ');
+    while (strpos($text, '  '))
+        $text = str_replace('  ', ' ', $text);
+    return $text;
+}
+
+$text = $_GET['text'];
+if ($text !== null)
+    if ($text === '')
+        echo 'Введите строку';
+    else
+        echo RemoveExtraBlanks($text);
+else
+    echo 'Введите данные text';
+
---
+++ lw3/task2/CheckIdentifier.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Content-Type: text/plain');
+function CheckIdent(string $text): void
+{
+    if (!ctype_alpha($text[0]))
+    {
+        echo 'no - идендификатор не может начинаться с ', $text[0];
+        return;
+    }
+    for ($i = 1; $i < strlen($text); $i++)
+    {
+        if(!ctype_alpha($text[$i]) && !is_numeric($text[$i]))
+        {
+            echo 'no - символ ', $text[$i], ' является недопустимым символом в идентификаторе';
+            return;
+        }
+    }
+    echo 'yes';
+}
+
+$text = $_GET['identifier'];
+if ($text !== null)
+    if ($text === '')
+        echo 'Введите строку';
+    else
+        CheckIdent($text);
+else
+    echo 'Введите данные';
